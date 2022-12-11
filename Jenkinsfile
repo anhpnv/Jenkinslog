762239b2235a69e498c5947c7a4ceeaa8d1640d3
Viet Anh Pham Nhu <nhuvietanh271098@gmail.com>
2022-12-11 23:53:27 +0700

    Initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,22 @@
+pipeline {
+    agent any
+    options {
+        ansiColor('xterm')
+    }
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    log = load "log.groovy"
+                }
+            }
+        }
+        stage('Test'){
+            steps {
+                script {
+                    log.info “Lorem ipsum dolor sit amet, consectetur adipiscing elit.”
+                }
+            }
+        }
+    }
+}
---
+++ log.groovy
@@ -0,0 +1,37 @@
+def loadColors(){
+    RED="\033[;31m"
+    BLUE="\033[;34m"
+    CYAN="\033[;36m"
+    GREEN="\033[;32m"
+    PURPLE="\033[;35m"
+    ON_YELLOW="\033[;43m"
+    NC="\033[0m"
+
+}
+
+def info(message) {
+    loadColors()
+    sh """
+    set +x; echo -e "${CYAN}[INFO] - $message  ${NC}"
+    """
+}
+
+def warn(message) {
+    loadColors()
+    sh """
+    set +x; echo -e "${PURPLE}[INFO] - $message  ${NC}"
+    """
+}
+
+def info(message) {
+    loadColors()
+}
+
+def debug(message) {
+    loadColors()
+    sh """
+    set +x; echo -e "${PURPLE}[INFO] - $message  ${NC}"
+    """
+}
+
+return this
